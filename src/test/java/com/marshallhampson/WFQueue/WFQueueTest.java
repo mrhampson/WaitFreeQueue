651dfd7e93aa08b0d0559919ef7ff95038ea0a05
Marshall Hampson <mhampson@liveaction.com>
2018-01-14 02:16:32 -0800

    Fixed gradle, add test stub

---
+++ src/test/java/com/marshallhampson/WFQueue/WFQueueTest.java
@@ -0,0 +1,19 @@
+package com.marshallhampson.WFQueue;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Marshall Hampson
+ */
+public class WFQueueTest {
+
+  @Test 
+  public void enqueue() {
+  }
+
+  @Test 
+  public void dequeue() {
+  }
+}
